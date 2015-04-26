587bf493f58da5df6a73c2e255f4b4fb36a6013c
Scott Morse <scott@gamechanger.io>
2015-04-25 17:43:37 -0700

    now with clickable footer bar

--- platforms/android/assets/www/js/main.js
+++ platforms/android/assets/www/js/main.js
@@ -1,4 +1,13 @@
 $(function() {
     FastClick.attach(document.body);
 
+    var OfferModel = Backbone.Model.extend({
+        initialize: function() {
+            this.set('name', 'An Offer');
+        };
+    });
+
+    var OfferCollection = Backbone.Collection.extend({
+
+    });
 });
--- www/test/browser_tests.js
+++ www/test/browser_tests.js
@@ -1,5 +1,5 @@
 //casperjs test www/test/browser_tests.js
-casper.options.clientScripts =  ['www/js/jquery-1.11.2.min.js'];
+casper.options.clientScripts =  ['www/js/jquery-1.11.2.min.js', 'www/test/sinon.js'];
 casper.options.timeout = 15000;
 casper.options.verbose = true;
 
@@ -10,15 +10,15 @@ var sizes = [
     {'name': 'ipad-landscape', 'width': 1024, 'height': 768}
 ];
 
-casper.test.begin('Test phone/table layouts', sizes.length, function suite(test) {
+casper.test.begin('Test phone/tablet layouts', sizes.length, function suite(test) {
     casper.start().each(sizes, function(self, size){
         self.then(function(){
             self.viewport(size.width, size.height);
         });
         self.thenOpen("file:///Users/Scott/Documents/WhereItsApp/www/index.html",
             function then(){
-                this.capture('test/images/main' + size.width + 'x' +size.height +'.png');
-                test.assertVisible('#page-header', 'Page Header is visible - ' + size.width +'x'+ size.height);
+                this.capture('www/test/images/index:' + size.name +'.png');
+                test.assertVisible('#page-header', 'Page Header is visible - ' + size.name);
             }
         );
     });
