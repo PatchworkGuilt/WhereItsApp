f9f120932481740a0357172d88b6933cf86fc213
Scott Morse <scottgmorse@gmail.com>
2015-06-29 17:24:37 -0700

    dont inject cordova.js

--- gulpfile.js
+++ gulpfile.js
@@ -164,9 +164,6 @@ gulp.task('html', function() {
   if (typeof config.weinre === 'object') {
     inject.push('<script src="http://'+config.weinre.boundHost+':'+config.weinre.httpPort+'/target/target-script-min.js"></script>');
   }
-  if (config.cordova) {
-    inject.push('<script src="cordova.js"></script>');
-  }
   gulp.src(['src/html/**/*.html'])
   .pipe(replace('<!-- inject:js -->', inject.join('\n    ')))
   .pipe(gulp.dest(config.dest));
